5398220ffbd85535af35bf7a97197a5afa45f96d
sven1103 <sven.fillinger@gmail.com>
2016-11-15 15:28:11 +0100

    Change folder structure

---
+++ src/main/java/life/qbic/portal/ManagerUI.java
@@ -0,0 +1,54 @@
+package life.qbic.portal;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * This UI is the application entry point. A UI may either represent a browser window 
+ * (or tab) or some part of a html page where a Vaadin application is embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * overridden to add component to the user interface and initialize non-component functionality.
+ */
+@Theme("mytheme")
+public class ManagerUI extends UI {
+
+    /**
+     * Get static logger instance
+     */
+    private final static Log log =
+            LogFactory.getLog(ManagerUI.class.getName());
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        log.info("Started application.");
+
+        final VerticalLayout layout = new VerticalLayout();
+
+        final ProjectContentModel model = new ProjectContentModel();
+
+        final ProjectOverviewModule projectOverviewModule = new ProjectOverviewModule(model);
+
+        layout.addComponent(projectOverviewModule);
+
+        layout.setMargin(true);
+        layout.setSpacing(true);
+
+
+        model.loadData();
+        setContent(layout);
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = ManagerUI.class, productionMode = false)
+    public static class ProjectManagerServlet extends VaadinServlet {
+    }
+}
