8f869f76ef39df8026896eb555de125ec0743077
sven1103 <sven.fillinger@gmail.com>
2016-12-05 15:10:39 +0100

    Add test class for the project overview module

---
+++ src/test/java/life.qbic.portal/ProjectOverviewModuleTest.java
@@ -0,0 +1,37 @@
+package life.qbic.portal;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by sven1103 on 5/12/16.
+ */
+public class ProjectOverviewModuleTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void getPresenter() throws Exception {
+
+    }
+
+    @Test
+    public void sendInfo() throws Exception {
+
+    }
+
+    @Test
+    public void sendError() throws Exception {
+
+    }
+
+    @Test
+    public void getOverviewGrid() throws Exception {
+
+    }
+
+}
