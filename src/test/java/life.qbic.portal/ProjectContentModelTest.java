775c44fab6dc862d7eec37ae6d99d6cf3f041e50
sven1103 <sven.fillinger@gmail.com>
2016-12-06 16:02:52 +0100

    Add test for getter and setter

--- src/test/java/life.qbic.portal/ProjectContentModelTest.java
+++ src/test/java/life.qbic.portal/ProjectContentModelTest.java
@@ -6,7 +6,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import java.sql.SQLException;
+
+import java.lang.reflect.Field;
 
 import static org.mockito.Mockito.*;
 
@@ -17,16 +18,28 @@ import static org.junit.Assert.*;
  */
 public class ProjectContentModelTest {
 
-    SimpleJDBCConnectionPool mockedConnectionPool = mock(SimpleJDBCConnectionPool.class);
-
     ProjectContentModel projectContentModel = new ProjectContentModel();
 
     FreeformQuery freeformQueryMock = mock(FreeformQuery.class);
 
+    @Test
+    public void setPassword() throws NoSuchFieldException, IllegalAccessException {
+        projectContentModel.setPassword("helloWorld");
 
+        final Field field = projectContentModel.getClass().getDeclaredField("password");
+        field.setAccessible(true);
+        assertEquals("Passwords don't match", field.get(projectContentModel), "helloWorld");
+    }
 
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
+    @Test
+    public void setUser() throws Exception {
+        projectContentModel.setUser("dummy");
+
+        final Field field = projectContentModel.getClass().getDeclaredField("user");
+        field.setAccessible(true);
+        assertEquals("Passwords don't match", field.get(projectContentModel), "dummy");
+    }
+
 
     @Test
     public void connectToDB() {
@@ -47,7 +60,7 @@ public class ProjectContentModelTest {
 
     @Test
     public void getTableContent() throws Exception {
-
+        assertNull(projectContentModel.getTableContent());
     }
 
     @Test
