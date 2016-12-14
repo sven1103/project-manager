048518eb708da2b8b99a9e7852da86eee1f31e68
sven1103 <sven.fillinger@gmail.com>
2016-12-14 13:52:33 +0100

    Add OpenBisConnection class

---
+++ src/main/java/life/qbic/portal/OpenBisConnection.java
@@ -0,0 +1,15 @@
+package life.qbic.portal;
+
+import life.qbic.openbis.openbisclient.OpenBisClient;
+
+/**
+ * Created by sven1103 on 8/12/16.
+ */
+public class OpenBisConnection {
+
+    //private final OpenBisClient openBisClient;
+
+    public boolean initConnection(String user, String password, String uri) {
+        return false;
+    }
+}
